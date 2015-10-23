1b63949238f18f78250fecedd15e5c0fd596d7a1
dfieds913@gmail.com <dfieds913@gmail.com>
2015-10-23 13:59:45 -0500

    Initial Commit

---
+++ SoundFuze/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SoundFuze
+//
+//  Created by Dylan Fiedler on 10/23/15.
+//  Copyright © 2015 xor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SoundFuzeTests/SoundFuzeTests.swift
@@ -0,0 +1,36 @@
+//
+//  SoundFuzeTests.swift
+//  SoundFuzeTests
+//
+//  Created by Dylan Fiedler on 10/23/15.
+//  Copyright © 2015 xor. All rights reserved.
+//
+
+import XCTest
+@testable import SoundFuze
+
+class SoundFuzeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
